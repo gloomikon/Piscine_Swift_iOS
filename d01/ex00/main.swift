4504d58c7f1eda3e4871f0fb6f8321f12adc0f74
gloomikon <gloomikon@gmail.com>
2019-10-01 18:32:25 +0300

    ex00

---
+++ d01/ex00/main.swift
@@ -0,0 +1,17 @@
+//
+//  Main.swift
+//  
+//
+//  Created by Mykola ZHURBA on 10/1/19.
+//
+//
+
+for color in Color.allColors {
+    print("Color \(color)\tis \(color.rawValue)")
+}
+
+print()
+
+for value in Value.allValues {
+    print("Value \(value)\tis \(value.rawValue)")
+}
